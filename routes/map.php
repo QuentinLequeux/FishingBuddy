f2ecda21195296c64ac66f5272af63c0b3581b39
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-10-18 21:48:02 +0200

    Update : routes/map.php

--- routes/map.php
+++ routes/map.php
@@ -7,4 +7,6 @@ Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/map', [MapController::class, 'show'])->name('map');
 
     Route::post('/map', [MapController::class, 'store'])->name('map.store');
+
+    Route::delete('/map/{spot}', [MapController::class, 'destroy'])->name('map.destroy');
 });
