95568f2c1807423265f46c8b69c9b6ac5e5fe873
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-10-18 22:00:11 +0200

    Add : create_spots_table migration

---
+++ database/migrations/2025_10_10_074712_create_spots_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('spots', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->decimal('latitude', 10, 7);
+            $table->decimal('longitude', 10, 7);
+            $table->string('name');
+            $table->string('license');
+            $table->string('url')->nullable();
+            $table->json('environement')->nullable();
+            $table->json('rules')->nullable();
+            $table->boolean('is_public')->default(true);
+            $table->json('equipments')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('spots');
+    }
+};
