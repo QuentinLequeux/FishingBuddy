1e4378d9d99902a8809e8cc13219ce6f9319a9f0
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-10-19 22:02:52 +0200

    Update : database/factories/SpotFactory.php

--- database/factories/SpotFactory.php
+++ database/factories/SpotFactory.php
@@ -17,7 +17,16 @@ class SpotFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'user_id' => 1,
+            'latitude' => 50.61153,
+            'longitude' => 5.51042,
+            'name' => 'Meuse',
+            'license' => 'Les amis de la Mehaigne',
+            'url' => 'https://www.lesamisdelamehaigne.be',
+            'environement' => ['Rocher', 'Sable'],
+            'rules' => ['No kill', 'Pêche de nuit'],
+            'is_public' => true,
+            'equipments' => ['Parking'],
         ];
     }
 }
