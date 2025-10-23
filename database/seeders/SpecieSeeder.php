f180701853a0266cfad2d5864bc1e2b7ddf59b47
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-10-23 21:06:06 +0200

    Add : database/seeders/SpecieSeeder.php

---
+++ database/seeders/SpecieSeeder.php
@@ -0,0 +1,47 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Specie;
+use Illuminate\Database\Seeder;
+
+class SpecieSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        $species = [
+            'Ablette',
+            'Anguille',
+            'Barbeau',
+            'Brème',
+            'Brochet',
+            'Carassin',
+            'Carpe',
+            'Chevaine',
+            'Épinoche',
+            'Gardon',
+            'Goujon',
+            'Hotu',
+            'Ide Melanote',
+            'Loche',
+            'Ombre',
+            'Perche',
+            'Perche soleil',
+            'Rotengle',
+            'Sandre',
+            'Silure',
+            'Tanche',
+            'Truite arc en ciel',
+            'Truite fario',
+            'Vairon',
+            'Vandoise',
+        ];
+
+        foreach ($species as $specie) {
+            Specie::create(['name' => $specie]);
+        }
+    }
+}
