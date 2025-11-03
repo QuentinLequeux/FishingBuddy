2923ee57f3ff5263411614a74867413758212999
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-11-03 21:52:52 +0100

    Add : resources/js/components/map/AlertDialogSpot.vue

---
+++ resources/js/components/map/AlertDialogSpot.vue
@@ -0,0 +1,88 @@
+<script setup lang="ts">
+import { route } from 'ziggy-js';
+import { toast } from 'vue-sonner';
+import {
+    AlertDialog,
+    AlertDialogTitle,
+    AlertDialogAction,
+    AlertDialogCancel,
+    AlertDialogFooter,
+    AlertDialogHeader,
+    AlertDialogContent,
+    AlertDialogTrigger,
+    AlertDialogDescription,
+} from '@/components/ui/alert-dialog';
+import { ISpot } from '@/types/ISpot';
+import { Form } from '@inertiajs/vue3';
+import { Trash2 } from 'lucide-vue-next';
+import { computed, defineProps } from 'vue';
+import { Inertia } from '@inertiajs/inertia';
+import { Button } from '@/components/ui/button';
+import MapController from '@/actions/App/Http/Controllers/Map/MapController';
+
+const props = defineProps<{
+    spot: ISpot | null;
+}>();
+
+const emit = defineEmits<{
+    (e: 'deleted', id: number | null): void;
+}>();
+
+const submit = computed(() => {
+    if (!props.spot) return {};
+    return MapController.destroy.form(props.spot.id);
+});
+
+const onSuccess = () => {
+    const id = props.spot?.id ?? null;
+
+    toast.success('Spot supprimé', {
+        duration: 5000,
+        action: {
+            label: 'Annuler',
+            onClick: () => {
+                if (!id) return;
+                Inertia.post(route('map.restore', { spot: id }));
+            },
+        },
+    });
+    emit('deleted', id);
+};
+</script>
+<template>
+    <AlertDialog>
+        <AlertDialogTrigger as-child>
+            <Button type="button" title="Supprimer" variant="ghost">
+                <Trash2 :size="20" />
+                Supprimer
+            </Button>
+        </AlertDialogTrigger>
+        <AlertDialogContent>
+            <AlertDialogHeader>
+                <AlertDialogTitle>Êtes-vous sûr&nbsp;?</AlertDialogTitle>
+                <AlertDialogDescription>Apr&egrave;s suppression, le spot ne sera plus disponible.</AlertDialogDescription>
+            </AlertDialogHeader>
+            <AlertDialogFooter>
+                <AlertDialogCancel>Annuler</AlertDialogCancel>
+                <Form
+                    v-bind="submit"
+                    @success="onSuccess"
+                >
+                    <AlertDialogAction as-child>
+                        <Button
+                            type="submit"
+                            title="Supprimer"
+                            class="bg-red-500 hover:bg-red-500/90"
+                        >
+                            Supprimer
+                        </Button>
+                    </AlertDialogAction>
+                </Form>
+            </AlertDialogFooter>
+        </AlertDialogContent>
+    </AlertDialog>
+</template>
+
+<!-- TODO : Transition -->
+<!-- TODO : Personnaliser toast -->
+<!-- TODO : Reload après suppression -->
