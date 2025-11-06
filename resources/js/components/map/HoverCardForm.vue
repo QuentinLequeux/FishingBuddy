fb3eeb6db6a247cb4ccc5442c9e57199d27e6af8
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-11-06 19:01:46 +0100

    Add : resources/js/components/map/HoverCardForm.vue

---
+++ resources/js/components/map/HoverCardForm.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import {
+    HoverCard,
+    HoverCardContent,
+    HoverCardTrigger,
+} from '@/components/ui/hover-card';
+import { Info } from 'lucide-vue-next';
+import { HoverCardArrow } from 'reka-ui';
+</script>
+<template>
+    <HoverCard>
+        <HoverCardTrigger>
+            <Info :size="16" class="text-white fill-main" />
+        </HoverCardTrigger>
+        <HoverCardContent>
+            <div class="text-xs">
+                <slot/>
+            </div>
+            <HoverCardArrow class="fill-white stroke-gray-300 dark:stroke-gray-950 dark:fill-gray-950" />
+        </HoverCardContent>
+    </HoverCard>
+</template>
+
+<!-- TODO : dark mode <HoverCard> -->
