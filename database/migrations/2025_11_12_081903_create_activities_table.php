4e667db0c99f9ebead69696464d8c953a0a069dc
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-11-13 17:24:14 +0100

    Add : database/migrations/2025_11_12_081903_create_activities_table.php

---
+++ database/migrations/2025_11_12_081903_create_activities_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('activities', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('specie');
+            $table->integer('size');
+            $table->decimal('weight');
+            $table->string('lure');
+            //$table->string('visibility');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('activities');
+    }
+};
+
+// TODO : Visibility ?, Lieu ?, Views ?
