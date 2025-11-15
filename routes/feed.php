a58aa9c8b62ddca734e9be7ca328b9f4eb6463cc
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-11-15 17:16:58 +0100

    Update : Add store, destroy and like routes in routes/feed.php

--- routes/feed.php
+++ routes/feed.php
@@ -5,7 +5,13 @@ use App\Http\Controllers\Activities\ActivitiesController;
 Route::middleware(['auth', 'verified'])->group(function () {
     Route::get('/feed', [ActivitiesController::class, 'index'])->name('feed');
 
+    Route::post('/feed', [ActivitiesController::class, 'store'])->name('feed.store');
+
     Route::post('/feed/{user}/follow', [ActivitiesController::class, 'follow'])->name('feed.follow');
+
+    Route::delete('/feed/{activity}', [ActivitiesController::class, 'destroy'])->name('feed.destroy');
+
+    Route::post('/feed/{activity}', [ActivitiesController::class, 'like'])->name('feed.like');
 });
 
 // TODO : Route accessible guest ?
