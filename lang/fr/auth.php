a5bf793ec0f61dfacac4caac6876a5eada62249d
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-11-16 19:15:38 +0100

    Update : lang/fr/auth.php

--- lang/fr/auth.php
+++ lang/fr/auth.php
@@ -13,8 +13,7 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
+    'failed' => 'Cet email ne correspond pas à un email existant.',
     'password' => 'The provided password is incorrect.',
     'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
-
 ];
