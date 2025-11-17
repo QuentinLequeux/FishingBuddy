00ec504f19e0c3cd0f89b1acfc0db919816186de
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-11-17 17:27:42 +0100

    Add : app/Models/Activity.php

---
+++ app/Models/Activity.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+
+class Activity extends Model
+{
+    /** @use HasFactory<\Database\Factories\ActivityFactory> */
+    use HasFactory;
+    use softDeletes;
+
+    protected $fillable = [
+        'user_id',
+        'specie',
+        'size',
+        'weight',
+        'lure',
+    ];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function likes()
+    {
+        return $this->hasMany(Like::class, 'activity_id');
+    }
+}
