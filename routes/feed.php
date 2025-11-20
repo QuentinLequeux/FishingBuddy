075ad21b262ee46683a158fc14672ffa9010c46e
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-11-20 20:06:42 +0100

    Update : Accessible route /feed for guests

--- routes/feed.php
+++ routes/feed.php
@@ -2,16 +2,15 @@
 
 use App\Http\Controllers\Activities\ActivitiesController;
 
+Route::get('/feed', [ActivitiesController::class, 'index'])->name('feed');
+
 Route::middleware(['auth', 'verified'])->group(function () {
-    Route::get('/feed', [ActivitiesController::class, 'index'])->name('feed');
 
     Route::post('/feed', [ActivitiesController::class, 'store'])->name('feed.store');
 
-    Route::post('/feed/{user}/follow', [ActivitiesController::class, 'follow'])->name('feed.follow');
+    Route::post('/feed/{activity}', [ActivitiesController::class, 'like'])->name('feed.like');
 
     Route::delete('/feed/{activity}', [ActivitiesController::class, 'destroy'])->name('feed.destroy');
 
-    Route::post('/feed/{activity}', [ActivitiesController::class, 'like'])->name('feed.like');
+    Route::post('/feed/{user}/follow', [ActivitiesController::class, 'follow'])->name('feed.follow');
 });
-
-// TODO : Route accessible guest ?
