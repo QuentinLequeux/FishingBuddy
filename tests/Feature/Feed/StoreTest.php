820bd4fcc23ead82d3b4d6f375617ffac3584ae7
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-11-21 19:57:46 +0100

    Add : tests/Feature/Feed/StoreTest.php

---
+++ tests/Feature/Feed/StoreTest.php
@@ -0,0 +1,34 @@
+<?php
+
+use App\Models\User;
+use App\Models\Lure;
+use App\Models\Specie;
+use Database\Seeders\LuresSeeder;
+use Database\Seeders\SpecieSeeder;
+
+test('user can create a post', function () {
+    $user = User::factory()->create();
+    $this->seed(SpecieSeeder::class);
+    $specie = Specie::first();
+    $this->seed(LuresSeeder::class);
+    $lure = Lure::first();
+
+    $payload = [
+        'specie_id' => $specie->id,
+        'size' => '60',
+        'weight' => '10',
+        'lure_id' => $lure->id,
+    ];
+
+    $response = $this->actingAs($user)->post(route('feed.store'), $payload);
+
+    $response->assertRedirect(route('feed'));
+
+    $this->assertDatabaseHas('activities', [
+        'user_id' => $user->id,
+        'specie_id' => $specie->id,
+        'size' => '60',
+        'weight' => '10',
+        'lure_id' => $lure->id,
+    ]);
+});
