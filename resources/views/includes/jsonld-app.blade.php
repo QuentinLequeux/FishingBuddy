9eef0848f2ceb120d419b2a9c0689a7ea65e2c07
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-11-29 22:35:48 +0100

    Add : resources/views/includes/jsonld-app.blade.php

---
+++ resources/views/includes/jsonld-app.blade.php
@@ -0,0 +1,21 @@
+@verbatim
+    <script type="application/ld+json">
+        {
+            "@context": "https://schema.org",
+                "@type": "Organization",
+                "name": "FishingBuddy",
+                "foundingDate": "2025",
+                "description": "FishingBuddy - Un compagnon pour la pêche !",
+                "sameAs": [
+                    "https://github.com/QuentinLequeux",
+                    "https://www.linkedin.com/in/quentin-lequeux-8a11a3192/"
+                ],
+                "alumni": [
+                    {
+                        "@type": "Person",
+                        "name": "Quentin Lequeux"
+                    }
+                ]
+        }
+    </script>
+@endverbatim
