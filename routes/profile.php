4e1a237d88b6ae210d8e8896f3426fc4c43f6a42
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-11-29 22:50:35 +0100

    Update : refactor routes/profile.php

--- routes/profile.php
+++ routes/profile.php
@@ -3,12 +3,21 @@
 use Inertia\Inertia;
 use App\Models\User;
 
+Route::get('/profile', function () {
+    return redirect('/');
+});
+
 Route::get('/profile/{user:name?}', function (User $user) {
     return Inertia::render('profile/Profile', [
         'user' => $user,
         'posts' => $user->activities()->count(),
         'follows' => $user->followers()->count(),
-        'isFollowing' => $user->following()->where('followed_id', $user->id)->exists(),
+        'followings' => $user->following()->count(),
+        'followers_list' => $user->followers()->with('follower:id,name')->get()->pluck('follower'),
+        'following_list' => $user->following()->with('followed:id,name')->get()->pluck('followed'),
+        'isFollowing' => auth()->check()
+            ? auth()->user()->following()->where('followed_id', $user->id)->exists()
+            : false,
     ]);
 })->name('profile');
 
