132f2b671d3c34138d328a4faa4206f6242f77d5
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-11-29 23:28:22 +0100

    Add : app/Http/Controllers/Activities/CommentController.php

---
+++ app/Http/Controllers/Activities/CommentController.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace App\Http\Controllers\Activities;
+
+use App\Models\Comment;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
+
+class CommentController extends Controller
+{
+    use AuthorizesRequests;
+
+    public function store(Request $request)
+    {
+        $validated = $request->validate([
+            'activity_id' => 'required|exists:activities,id',
+            'content' => 'required|string|min:3|max:1000'
+        ]);
+
+        Comment::create([
+            'user_id' => auth()->user()->id,
+            'activity_id' => $validated['activity_id'],
+            'content' => $validated['content'],
+        ]);
+
+        return redirect()->route('feed');
+    }
+
+    public function destroy(Comment $comment)
+    {
+        $this->authorize('delete', $comment);
+
+        $comment->delete();
+
+        return redirect()->route('feed');
+    }
+}
