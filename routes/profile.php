d5937f6fce4c99c325bf31802f6860a5dcbe91c1
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-12-01 22:22:17 +0100

    Update : slug in routes/profile.php

--- routes/profile.php
+++ routes/profile.php
@@ -7,18 +7,16 @@ Route::get('/profile', function () {
     return redirect('/');
 });
 
-Route::get('/profile/{user:name?}', function (User $user) {
+Route::get('/profile/{user:slug}', function (User $user) {
     return Inertia::render('profile/Profile', [
         'user' => $user,
         'posts' => $user->activities()->count(),
         'follows' => $user->followers()->count(),
         'followings' => $user->following()->count(),
-        'followers_list' => $user->followers()->with('follower:id,name')->get()->pluck('follower'),
-        'following_list' => $user->following()->with('followed:id,name')->get()->pluck('followed'),
+        'followers_list' => $user->followers()->with('follower:id,name,slug')->get()->pluck('follower'),
+        'following_list' => $user->following()->with('followed:id,name,slug')->get()->pluck('followed'),
         'isFollowing' => auth()->check()
             ? auth()->user()->following()->where('followed_id', $user->id)->exists()
             : false,
     ]);
 })->name('profile');
-
-// TODO : slug
