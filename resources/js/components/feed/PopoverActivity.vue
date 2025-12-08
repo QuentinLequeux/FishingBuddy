71f94cdba8dfad7d1687cc4159dc4b3576f8a052
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-12-08 20:47:51 +0100

    Add : resources/js/components/feed/PopoverActivity.vue

---
+++ resources/js/components/feed/PopoverActivity.vue
@@ -0,0 +1,44 @@
+<script setup lang="ts">
+import {
+    Popover,
+    PopoverContent,
+    PopoverTrigger,
+} from '@/components/ui/popover';
+import { route } from 'ziggy-js';
+import { Form } from '@inertiajs/vue3';
+import { PopoverArrow } from 'reka-ui';
+import { Button } from '@/components/ui/button';
+import { IActivities } from '@/types/IActivities';
+import { Ellipsis, SquarePen, Trash2Icon } from 'lucide-vue-next';
+
+defineProps<{
+    activity: IActivities;
+}>();
+</script>
+
+<template>
+    <Popover>
+        <PopoverTrigger>
+            <Ellipsis class="cursor-pointer" />
+        </PopoverTrigger>
+        <PopoverContent class="w-fit p-2">
+            <Button variant="ghost" title="Modifier">
+                <SquarePen />
+                Modifier
+            </Button>
+            <Form :action="route('feed.destroy', activity.id)" method="delete">
+                <Button
+                    variant="ghost"
+                    title="Supprimer"
+                    class="text-red-500 hover:text-red-500"
+                >
+                    <Trash2Icon />
+                    Supprimer
+                </Button>
+            </Form>
+            <PopoverArrow
+                class="fill-white stroke-gray-300 dark:fill-gray-950 dark:stroke-gray-700"
+            />
+        </PopoverContent>
+    </Popover>
+</template>
