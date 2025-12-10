01e7e3bb74ffb11c55603d309edba13949a834da
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-12-10 21:52:33 +0100

    Update : refactor routes/profile.php

--- routes/profile.php
+++ routes/profile.php
@@ -13,8 +13,11 @@ Route::get('/profile/{user:slug}', function (User $user) {
         'posts' => $user->activities()->count(),
         'follows' => $user->followers()->count(),
         'followings' => $user->following()->count(),
+        'old' => $user->activities()->with(['specie', 'lure', 'user', 'comments.user'])->oldest()->get(),
+        'recent' => $user->activities()->with(['specie', 'lure', 'user', 'comments.user'])->latest()->get(),
         'followers_list' => $user->followers()->with('follower:id,name,slug')->get()->pluck('follower'),
         'following_list' => $user->following()->with('followed:id,name,slug')->get()->pluck('followed'),
+        'liked' => $user->activities()->with(['specie', 'lure', 'user', 'comments.user'])->withCount('likes')->orderByDesc('likes_count')->get(),
         'isFollowing' => auth()->check()
             ? auth()->user()->following()->where('followed_id', $user->id)->exists()
             : false,
