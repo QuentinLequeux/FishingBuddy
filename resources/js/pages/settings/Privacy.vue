3914b29a82d3827e4f8d85dcfa4476fee56272d5
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-12-15 17:04:14 +0100

    Add : resources/js/pages/settings/Privacy.vue

---
+++ resources/js/pages/settings/Privacy.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import { Head } from '@inertiajs/vue3';
+import { edit } from '@/routes/privacy';
+import type { BreadcrumbItem } from '@/types';
+import AppLayout from '@/layouts/AppLayout.vue';
+import HeadingSmall from '@/components/HeadingSmall.vue';
+import SettingsLayout from '@/layouts/settings/Layout.vue';
+
+const breadcrumbItems: BreadcrumbItem[] = [
+    {
+        title: 'Paramètres de confidentialité',
+        href: edit().url,
+    },
+];
+</script>
+
+<template>
+    <AppLayout :breadcrumbs="breadcrumbItems">
+        <Head title="Paramètres de confidentialité" />
+        <SettingsLayout>
+            <div class="space-y-6">
+                <HeadingSmall
+                    title="Paramètres de confidentialité"
+                    description="Mettre à jour les paramètres de confidentialité de votre compte"
+                />
+            </div>
+        </SettingsLayout>
+    </AppLayout>
+</template>
