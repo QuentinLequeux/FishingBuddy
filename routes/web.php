cf5f6e41a6c4200fc0b1f295d49436bf8884e083
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-12-15 21:18:34 +0100

    Update : refactor routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -11,9 +11,21 @@ Route::get('/', function () {
 
 Route::get('dashboard', function () {
     $user = auth()->user();
-    $users = User::inRandomOrder()->take(4)->get();
+    $users = User::inRandomOrder()->take(4)->get()->map(function ($u) use ($user) {
+        $u->isFollowing = $user ? $user->following()->where('followed_id', $u->id)->exists() : false;
+        return $u;
+    });
     $activity = Activity::with(['user', 'specie', 'lure', 'comments.user'])->inRandomOrder()->first();
-    return Inertia::render('Dashboard', ['user' => $user, 'users' => $users, 'activity' => $activity]);
+    if ($activity && $user) {
+    $activity->isFollowing = $user
+        ->following()
+        ->where('followed_id', $activity->user_id)
+        ->exists();}
+    return Inertia::render('Dashboard', [
+        'user' => $user,
+        'users' => $users,
+        'activity' => $activity,
+    ]);
 })->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/map.php';
