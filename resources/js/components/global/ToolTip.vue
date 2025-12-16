ae6dede026603cdf0de9dde0b7af9d8ffad6ab44
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-12-16 16:50:45 +0100

    Add : resources/js/components/global/ToolTip.vue

---
+++ resources/js/components/global/ToolTip.vue
@@ -0,0 +1,29 @@
+<script setup lang="ts">
+import {
+    Tooltip,
+    TooltipContent,
+    TooltipTrigger,
+    TooltipProvider,
+} from '@/components/ui/tooltip';
+
+defineProps<{
+    label?: string | number,
+}>();
+</script>
+
+<template>
+    <TooltipProvider>
+        <Tooltip>
+            <TooltipTrigger as-child>
+                <p class="cursor-help text-xs">
+                    <slot />
+                </p>
+            </TooltipTrigger>
+            <TooltipContent>
+                <slot/>
+            </TooltipContent>
+        </Tooltip>
+    </TooltipProvider>
+</template>
+
+<!-- TODO : Utilisation générale ? -->
