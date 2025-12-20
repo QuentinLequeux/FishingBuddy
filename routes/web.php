3ebe9ba5ce2edf168226a6ea2d400813d7003a4c
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-12-20 22:33:46 +0100

    Update : refactor routes/web.php

--- routes/web.php
+++ routes/web.php
@@ -1,35 +1,19 @@
 <?php
 
-use App\Models\User;
 use Inertia\Inertia;
-use App\Models\Activity;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\DashboardController;
 
 Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('dashboard', function () {
-    $user = auth()->user();
-    $users = User::inRandomOrder()->take(4)->get()->map(function ($u) use ($user) {
-        $u->isFollowing = $user ? $user->following()->where('followed_id', $u->id)->exists() : false;
-        return $u;
-    });
-    $activity = Activity::with(['user', 'specie', 'lure', 'comments.user'])->inRandomOrder()->first();
-    if ($activity && $user) {
-    $activity->isFollowing = $user
-        ->following()
-        ->where('followed_id', $activity->user_id)
-        ->exists();}
-    return Inertia::render('Dashboard', [
-        'user' => $user,
-        'users' => $users,
-        'activity' => $activity,
-    ]);
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::get('dashboard', [DashboardController::class, 'index'])->middleware(['auth', 'verified'])->name('dashboard');
 
 require __DIR__.'/map.php';
 require __DIR__.'/feed.php';
 require __DIR__.'/auth.php';
 require __DIR__.'/profile.php';
 require __DIR__.'/settings.php';
+
+// TODO : Middleware verified
