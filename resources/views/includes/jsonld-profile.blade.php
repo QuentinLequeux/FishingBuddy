f076b137b92e33f6f0cad370338790fcbd76414f
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-12-21 22:35:39 +0100

    Update : refactor resources/views/includes/jsonld-profile.blade.php

--- resources/views/includes/jsonld-profile.blade.php
+++ resources/views/includes/jsonld-profile.blade.php
@@ -1,8 +1,16 @@
-@verbatim
-    <script type="application/ld+json">
+@php
+    $user = request()->route('user');
+    $url = url()->current();
+    $asset = asset('storage/' . $user->avatar);
+    echo <<<d
+        <script type="application/ld+json">
         {
             "@context": "https://schema.org",
                 "@type": "Person",
+                "alternateName": "$user->name",
+                "url": "$url",
+                "image": "$asset"
         }
-    </script>
-@endverbatim
+        </script>
+        d;
+@endphp
