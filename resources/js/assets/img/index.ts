75f2ca96babc579541f7bce3a93fa19d64a48c8b
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-12-22 23:02:13 +0100

    Add : resources/js/assets/img/index.ts

---
+++ resources/js/assets/img/index.ts
@@ -0,0 +1,8 @@
+// Static images for the welcome page.
+
+import map from './map.png';
+import feed from './feed.png';
+import hero from './hero.png';
+import hero2 from './hero2.png';
+
+export { map, feed, hero, hero2 };
