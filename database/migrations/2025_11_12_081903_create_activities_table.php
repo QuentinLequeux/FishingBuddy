d01efe23d9e354806e863a60a362951b694a7f8e
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-12-23 21:53:02 +0100

    Update : Add cascadeOnDelete in database/migrations/2025_11_12_081903_create_activities_table.php

--- database/migrations/2025_11_12_081903_create_activities_table.php
+++ database/migrations/2025_11_12_081903_create_activities_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('activities', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('specie_id')->constrained('species');
             $table->integer('size');
             $table->decimal('weight');
