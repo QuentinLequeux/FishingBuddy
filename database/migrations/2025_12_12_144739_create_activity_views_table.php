feb4b9135f202334cbad7182ad429b5dbff6e3a4
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-12-23 21:57:23 +0100

    Update : Add cascadeOnDelete in databse/migrations/2025_12_12_144739_create_activity_views_table.php

--- database/migrations/2025_12_12_144739_create_activity_views_table.php
+++ database/migrations/2025_12_12_144739_create_activity_views_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('activity_views', function (Blueprint $table) {
             $table->id();
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->foreignId('activity_id')->constrained();
             $table->timestamps();
 
