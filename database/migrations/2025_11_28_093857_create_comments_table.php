eb5f7db356121b1f13c0ec2b615aba4e9effa3eb
QuentinLequeux <145542949+QuentinLequeux@users.noreply.github.com>
2025-12-23 21:55:58 +0100

    Update : Add cascadeOnDelete in database/migrations/2025_11_28_093857_create_comments_table.php

--- database/migrations/2025_11_28_093857_create_comments_table.php
+++ database/migrations/2025_11_28_093857_create_comments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->foreignId('activity_id')->constrained('activities');
-            $table->foreignId('user_id')->constrained();
+            $table->foreignId('user_id')->constrained()->cascadeOnDelete();
             $table->text('content');
             $table->timestamps();
         });
